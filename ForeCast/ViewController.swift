4ac0d0074c79d319ac9a8fa76b9aa467fea98274
Srini Motheram <srini@srinimoeramsmbp.wowway.com>
2017-03-21 00:26:24 -0400

    monday night 1224

--- ForeCast/ViewController.swift
+++ ForeCast/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class ViewController: UIViewController {
 
-    
-  //  var museumArray = [MuseumItem]()
-    
+   // two empty objects to be filled later.
+  let currLoc = Location(name: "currentLocation", summary: "unknown", icon: "default", lat: 0.0, lon: 0.0, temp: 0.0, humidity: 0.0, wind: 0.0)
+  let searchedLoc = Location(name: "searchedLocation", summary: "unknown", icon: "default", lat: 0.0, lon: 0.0, temp: 0.0, humidity: 0.0, wind: 0.0)
     //MARK :- LIFE CYCLE METHODS
     
     let hostName = "api.darksky.net"
@@ -24,39 +25,106 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var skey = "21b2256204277dfbb5f66521c87eb4e2"
     
+    @IBOutlet var locationNameLabel    :UILabel!
+    @IBOutlet var summaryLabel          :UILabel!
+    @IBOutlet var tempLabel             :UILabel!
+    @IBOutlet var iconLabel              :UILabel!
+    @IBOutlet var humidityLabel          :UILabel!
+    @IBOutlet var windspeedLabel         :UILabel!
+    
     @IBOutlet var networkStatusLabel    :UILabel!
     @IBOutlet var searchField           :UITextField!
     @IBOutlet var forecastTableView        :UITableView!
+    var locationMgr = CLLocationManager()
+    
+    //MARK:- geo coding methods
+    @IBAction func latlonSearch(button: UIButton){
+        print(searchField.text!)
+        searchField.resignFirstResponder()
+        guard let searchText = searchField.text, let loc = locationFrom(string: searchText) else {
+            return
+        }
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(loc){(placemarks, error) in
+            if let err = error {
+                print ("Got error \(err.localizedDescription)")
+            } else {
+               // self.addLocAndPinFor(placemarks: placemarks, title: "From Lat/Lon: \(searchText)")
+                // print("lat: \(placemark.location!.coordinate.latitude), lon: \(placemark.location!.coordinate.longitude)")
+                
+            }
+            
+        }
+    }
+    
+    @IBAction func detroitSearch(button: UIButton){
+        searchField.resignFirstResponder()
+        guard let searchText = searchField.text else {
+            return
+        }
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(searchText) {(placemarks, error) in
+            if let err = error {
+                print ("Got error \(err.localizedDescription)")
+            } else {
+                //self.addLocAndPinFor(placemarks: placemarks, title: "From Address: \(searchText)")
+                //print("lat: \(placemarks.location!.coordinate.latitude), lon: \(placemarks.location!.coordinate.longitude)")
+                guard let placemark = placemarks?.first else {
+                    return
+                }
+                print("placemarks: \(placemark)")
+                 print(" lat: \(placemark.location!.coordinate.latitude), lon: \(placemark.location!.coordinate.longitude)")
+                
+            }
+        }
+    }
+
+    
+    
+    func locationFrom(string: String) -> CLLocation? {
+        let coordItems = string.components(separatedBy: ",")
+        if coordItems.count == 2 {
+            guard let lat = Double(coordItems[0]), let lon = Double(coordItems[1]) else {
+                return nil
+            }
+            print("lat: \(lat), lon:\(lon)")
+            return CLLocation(latitude: lat, longitude: lon)
+        }
+        return nil
+    }
+    
     
     //MARK :- CORE METHODS
     // not using the following func
     func parseJason(data: Data){
         
         do {
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSDictionary //[String:Any]
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
            // print("JSON: \(jsonResult)")
             
          //   let currentWeather = jsonResult["currently"] as? [String:Any] ?? nil
             
-            if let currentWeatherDict = jsonResult["currently"] as? NSDictionary {
+            if let currentWeatherDict = jsonResult["currently"] as? [String:Any] {
                 
-            let temp = currentWeatherDict["temperature"] as? String ?? "no data"
-            let time = currentWeatherDict["time"] as? String ?? "no data"
-                print("currently: \(currentWeatherDict), time:\(time) , temp: \(temp)")
+            let temp = currentWeatherDict["temperature"] as? Double ?? 0.0
+            let humidity = currentWeatherDict["humidity"] as? Double ?? 0.0
+            let windspeed = currentWeatherDict["windSpeed"] as? Double ?? 0.0
+             let summary = currentWeatherDict["summary"] as? String ?? "no data"
+             let icon = currentWeatherDict["icon"] as? String ?? "no data"
+             //   print("currently: \(currentWeatherDict)")
+                print("summary:\(summary) , temp: \(temp), icon: \(icon)")
+                print("humidity: \(humidity), windspeed: \(windspeed)")
+            currLoc.weatherSummary = summary
+            currLoc.temp = temp
+            currLoc.icon = icon
+            currLoc.humidity = humidity
+            currLoc.windspeed = windspeed
+                
+                updateUI(loc: currLoc)
                 
             }
-            
-            
-          //  let icon = jsonResult["timezone"] as? String ?? "no data"
-            
-           /* let flavorsArray = jsonResult["temperature"] as! [[String:Any]]
-            for flavorDict in flavorsArray {
-                print("Flavor:\(flavorDict)")
-            }
-            for flavorDict in flavorsArray {
-                print("Flavor:\(flavorDict["summary"])")
-            } */
-            
+
         } catch {
             print("JSON Parsing Error")
         }
@@ -66,37 +134,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    func parseItunesJason(data: Data){
-        
-        do {
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSArray
-            //   [String:Any]()
-            // print("JSON: \(jsonResult)")
-            let firstRow = jsonResult[0]
-            print("first row: \(firstRow)")
-            let museumArray2 = jsonResult as! [[String:Any]]
-            
-            for museumDict in museumArray2 {
-                print("museum:\(museumDict["commonname"])")
-                
-             /*   let museumName2 = museumDict["commonname"] as? String ?? "no data"
-                let street2 = museumDict["location_1_address"] as? String ?? "no street data"
-                let city2 = museumDict["location_1_city"] as? String ?? "no city data"
-                let state2 = museumDict["location_1_state"] as? String ?? "no state data" */
-                
-             //   museumArray.append(MuseumItem(museumName: museumName2, street: street2, city: city2,  state: state2))
-                
-            }
-            
-            //  print("Museum Array: \(museumArray)")
-        }
-        catch {
-            print("JSON Parsing Error")
-        }
-        DispatchQueue.main.async {
-            self.forecastTableView.reloadData()
-            UIApplication.shared.isNetworkActivityIndicatorVisible = false
-        }
+    func updateUI(loc: Location){
+        locationNameLabel.text = loc.locationName
+        summaryLabel.text = "summary: \(loc.weatherSummary!)"
+        tempLabel.text = "temp: \(loc.temp!)"
+        iconLabel.text = "icon: \(loc.icon!)"
+        humidityLabel.text = "humidity: \(loc.humidity!)"
+        windspeedLabel.text = "wind: \(loc.windspeed!)"
         
     }
     
@@ -131,60 +175,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         task.resume()
     }
     
-    //MARK: - setup METHODS -- just for testing
- /*   func fillArray() -> [MuseumItem]{
-        let museum3 = MuseumItem(museumName: "Bill Museum", street: "123 oak", city: "ypsi", state: "FL")
-        let museum2 = MuseumItem(museumName: "Joe Museum", street: "123 maple", city: "canton", state: "FL")
-        return [museum3, museum2]
-    } */
-    
-    
   //  @IBAction func getFilePressed(button: UIButton){
-    func getFilePressed(){
+    func getFilePressed(location: Location){
         guard let reach = reachability else {
             return
         }
         
         if reach .isReachable {
             // getFile(filename: "/classfiles/iOS_URL_Class_Get_File.txt")
-            getFile(filename: "/forecast/21b2256204277dfbb5f66521c87eb4e2/37.8267,-122.4233")
+            getFile(filename: "/forecast/21b2256204277dfbb5f66521c87eb4e2/" + "\(location.locationLat!)" + "," + "\(location.locationLon!)")
+            // getFile(filename: "/forecast/21b2256204277dfbb5f66521c87eb4e2/37.8267,-122.4233")
             // getFile(filename: "/search?term=\(searchTerm)")
             
         } else {
             print("Host Not reachable. Turn on the internet")
         }
         
-        
     }
-    
-    //MARK :- Table View Methods
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      //  return museumArray.count
-        return 1
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) // as! MuseumTableViewCell
-       /* let currentMuseumItem = museumArray[indexPath.row]
-        cell.museumNameLabel.text = currentMuseumItem.museumName
-        cell.streetLabel.text = currentMuseumItem.street
-        cell.cityLabel.text = currentMuseumItem.city
-        cell.stateLabel.text = currentMuseumItem.state */
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80.0
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //let currentMuseumItem = museumArray[indexPath.row]
-       // print("Row: \(indexPath.row) \(currentMuseumItem.museumName)")
-    }
-    
     
     //MARK :- REACHABILITY METHODS
     
@@ -200,7 +207,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 self.updateLabel(reachable: false, reachability: reachability)
             }
         }
-        
     }
     
     func startReachability(){
@@ -234,14 +240,64 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         setupReachability(hostName: hostName)
         startReachability()
-        getFilePressed()
+        
         // albumArray = fillArray()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupLocationMonitoring()
+        getFilePressed(location: currLoc)
+        //buildArray()
+       // annotateMapLocations()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
 
+}
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLoc = locations.last!
+        print("Last Loc: \(lastLoc.coordinate.latitude),\(lastLoc.coordinate.longitude), description:\(lastLoc.description)")
+        currLoc.locationLat = lastLoc.coordinate.latitude
+        currLoc.locationLon = lastLoc.coordinate.longitude
+       // currLoc.locationName = lastLoc.description
+        
+       // zoomToLocation(lat: lastLoc.coordinate.latitude, lon: lastLoc.coordinate.longitude, radius: 500)
+        manager.stopUpdatingLocation()
+    }
+    
+    //MARK: - Location Authorization Methods
+    
+    func turnOnLocationMonitoring() {
+        locationMgr.startUpdatingLocation()
+       // coffeeMap.showsUserLocation = true
+    }
+    
+    func setupLocationMonitoring() {
+        locationMgr.delegate = self
+        locationMgr.desiredAccuracy = kCLLocationAccuracyBest
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedAlways, .authorizedWhenInUse:
+                turnOnLocationMonitoring()
+            case .denied, .restricted:
+                print("Hey turn us back on in Settings!")
+            case .notDetermined:
+                if locationMgr.responds(to: #selector(CLLocationManager.requestAlwaysAuthorization)) {
+                    locationMgr.requestAlwaysAuthorization()
+                }
+            }
+        } else {
+            print("Hey Turn Location On in Settings!")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        setupLocationMonitoring()
+    }
 }
 
